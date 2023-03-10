838733ef245b5bf084c85048886951d1145dba25
videovulture <30363299+videovulture@users.noreply.github.com>
2023-03-09 21:07:35 -0800

    907-contact

--- src/Contact.js
+++ src/Contact.js
@@ -3,14 +3,12 @@ import FooterV2 from './FooterV2';
 import Form from './Form';
 import NavBarLightV2 from './NavBarLightV2';
 import Locations from './Location';
-import Blog from './Blog';
 
 const Contact = () => {
   return (
     <div>
     <NavBarLightV2 />
     <Form />
-    <Blog />
     <FooterV2 />
     </div>
   )
